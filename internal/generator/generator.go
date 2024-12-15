09da04d6b21c49a038a03da849e1fe481aa55db7
Vinicius <145880205+ViniStef@users.noreply.github.com>
2024-12-15 14:50:06 -0300

    feat: white border on position squares and initial data direction logic

--- internal/generator/generator.go
+++ internal/generator/generator.go
@@ -6,6 +6,7 @@ type QRCode interface {
 	initializeMatrix()
 	addPositionSquares()
 	addIndicators()
+	addData()
 	render() string
 }
 
@@ -21,6 +22,7 @@ func (qrc QRCodeV1) InitializeMatrix() {
 
 	qrc.addPositionSquares()
 	qrc.addIndicators("www.google.com")
+	qrc.addData()
 
 	for i := 0; i < len(qrc.matrix); i++ {
 		fmt.Println(qrc.matrix[i])
@@ -28,33 +30,51 @@ func (qrc QRCodeV1) InitializeMatrix() {
 }
 
 func (qrc QRCodeV1) addPositionSquares() {
-	qrc.matrix = createSquare(qrc.matrix, 0, 0)
-	qrc.matrix = createSquare(qrc.matrix, 0, 14)
-	qrc.matrix = createSquare(qrc.matrix, 14, 0)
+	qrc.matrix = createSquare(qrc.matrix, 0, 0, "topLeft")
+	qrc.matrix = createSquare(qrc.matrix, 0, 14, "topRight")
+	qrc.matrix = createSquare(qrc.matrix, 14, 0, "botLeft")
 }
 
-func createSquare(matrix [][]int, rowStart, colStart int) [][]int {
-	bottomSquare := matrix
+func createSquare(matrix [][]int, rowStart, colStart int, position string) [][]int {
+	square := matrix
+
+	switch position {
+	case "topLeft":
+		for i := 0; i < 8; i++ {
+			square[rowStart+7][i] = 1
+			square[i][rowStart+7] = 1
+		}
+	case "topRight":
+		for i := 0; i < 8; i++ {
+			square[rowStart+7][i+colStart-1] = 1
+			square[i][colStart-1] = 1
+		}
+	case "botLeft":
+		for i := 0; i < 8; i++ {
+			square[rowStart-1][i] = 1
+			square[i+rowStart-1][colStart+7] = 1
+		}
+	}
 
 	for i := rowStart; i < rowStart+7; i++ {
 		for j := colStart; j < colStart+7; j++ {
 			if i != rowStart && i != (rowStart+7-1) && j != colStart && j != (colStart+7-1) {
 				if i >= rowStart+2 && i <= rowStart+4 && j >= colStart+2 && j <= colStart+4 {
 					if i == rowStart+3 && j == colStart+3 {
-						bottomSquare[i][j] = 0
+						square[i][j] = 1
 					} else {
-						bottomSquare[i][j] = 1
+						square[i][j] = 2
 					}
 				} else {
-					bottomSquare[i][j] = 0
+					square[i][j] = 1
 				}
 			} else {
-				bottomSquare[i][j] = 1
+				square[i][j] = 2
 			}
 		}
 	}
 
-	return bottomSquare
+	return square
 }
 
 func (qrc QRCodeV1) addIndicators(msg string) {
@@ -77,5 +97,55 @@ func (qrc QRCodeV1) addIndicators(msg string) {
 			qrc.matrix[i][j] = int(binaryMsgSize[countCurrBinary] - '0')
 			countCurrBinary++
 		}
+	}
+}
+
+func (qrc QRCodeV1) addData() {
+	matrix := qrc.matrix
+
+	currIteration := 0
+	startRow := len(matrix) - 1
+	startCol := len(matrix) - 1
+
+	goUpwards := true
+
+	for currIteration < (len(matrix) / 2) {
+		if goUpwards {
+			for i := startRow; i >= 0; i-- {
+				for j := startCol; j > startCol-2 && j >= 0; j-- {
+					if i < len(matrix)-6 {
+						if matrix[i][j] == 0 {
+							matrix[i][j] = 3
+						}
+					} else if startCol < len(matrix)-2 {
+						if matrix[i][j] == 0 {
+							matrix[i][j] = 3
+						}
+					}
+				}
+			}
+
+			goUpwards = false
+		} else {
+			testVar := 4
+			for i := 0; i <= startRow; i++ {
+				for j := startCol - 1; j <= startCol && j >= 0; j++ {
+					if matrix[i][j] == 0 {
+						matrix[i][j] = testVar
+						if testVar == 4 {
+							testVar = 5
+						} else {
+							testVar = 4
+						}
+					}
+				}
+			}
+
+			goUpwards = true
+		}
+
+		startCol = startCol - 2
+
+		currIteration++
 	}
 }
